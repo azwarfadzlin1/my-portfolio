016ad56352161bc0c0d801931043ea0cbffaa36e
azwar <azwarfadzlin1@gmail.com>
2026-05-13 23:30:57 +0800

    Updated Image for the project Section

--- app/page.tsx
+++ app/page.tsx
@@ -193,7 +193,7 @@ export default function Home() {
                    <div className="absolute inset-0 bg-linear-to-tr from-blue-500/10 to-purple-500/10 opacity-0 group-hover:opacity-100 transition-opacity"></div>
                    <MonitorPlay className="w-12 h-12 text-zinc-600" />
                    <span className="font-medium">UniMove UI / Video Placeholder</span>
-                   <img src="/unimove_logo.png" alt="UniMove Logo" />
+                   <Image src="/unimove_logo.png" alt="UniMove Logo" width={60} height={60} className="object-contain" />
                 </div>
               </div>
               <div className="p-8">
